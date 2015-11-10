db42086f9844473300dedb18405558b1c4fdd1c9
ecoronado89 <coreddin@gmail.com>
2015-11-10 16:30:22 -0600

    Validations for swatches

---
+++ PDPAutomation/src/defaultPackage/Selector.java
@@ -0,0 +1,30 @@
+package defaultPackage;
+
+/* Class that holds all the Strings required to run the tests
+ * and ease the access to different definitions */
+public final class Selector {
+
+	private Selector() {
+
+	}
+
+	// Key words
+	public static final String APP = "#alternateProductPage";
+	public static final String NAME = "[itemprop=name]";
+	public static final String ORDOPTCONT = "#data_0";
+	public static final String OLDSWATCHES = "//*[@id=\"colorSwatchContainer0_0_0\"]/ul/li[2]/a";
+	public static final String TABS_UL = "#ppItemTabs";
+	public static final String PROD_AVAIL = ".ppItemUnavailable";
+	public static final String ITM_PRICE = ".ppItemPriceContainer";
+	public static final String SOLD_OUT = ".toOrderItemSalePriceMarkdown";
+	public static final String SCHART = ".btn_size_chart";
+	public static final String NEWSCHART	= "View Size Chart";
+	public static final String BREADC = "#breadcrumbs";
+	public static final String COPY = "#ppDetails";
+	public static final String NEWCOPY = "//*[@id='pagecontent']/section[1]/div[1]/div[1]";
+
+	// HTML Tags
+	public static final String LI = "li";
+	public static final String IMG = "img";
+
+}
