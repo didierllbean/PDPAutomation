a8c706ef5c15bb2874bf5f23fea8bef1d31783b2
Didier Corrales <dcorrales@llbean.com>
2016-05-30 08:32:03 -0600

    Adding final version of the new automation to master

---
+++ PDPAutomation/src/defaultPackage/PDP.java
@@ -0,0 +1,16 @@
+package defaultPackage;
+
+/*Interface with the basic structure of the Product Pages */
+public interface PDP {
+
+	public String getPDPType();
+	public boolean inStock();
+	public boolean validateSizeChart();
+	public void VerifySwatches();
+	public boolean verifyImage(String page);
+	public boolean validateCopyExist();
+	public boolean validateBreadcrum();
+	public boolean isProductAvailable();
+	
+	
+}
